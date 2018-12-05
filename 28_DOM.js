41a123d3b2dd151e08586d13a0e3d027c1fe4e3f
Lea Vaiana <leavaiana@gmail.com>
2018-12-05 11:07:41 +0100

    File updated

--- 28_DOM.js
+++ 28_DOM.js
@@ -6,14 +6,16 @@
 //to get elements:
 var box = document.getElementById("myBox")
 
-//console.log(box); //it return div
+console.log(box); //it return div
+
+var box = document.getElementById("myBox")
 
 //since the element is already selected I can change it
 // box.innerHTML // Hi, I am a box
 
 //I can modify it:
 
-box.innerHTML = "A new text into the box"; //refresh page!
+box.innerHTML = "A new text into the box inserted";
 box.style.background = "pink";
 box.style.padding = "20 px";
 box.style.color = "green";
@@ -24,5 +26,9 @@ function changeColor(color){
     box.style.background = color;
 }
 
+changeColor("pink")//posso invocare la funzione per cambiare il colore
 console.log(changeColor);
+
+//Query Selector, una alternativa per selezionare
+var box = document.querySelector("#myBox"); //name per il nome dell'elemento, #name for ID element, .name for class element
 
