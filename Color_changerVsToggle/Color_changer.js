baa9f035202a2266ef4c318ae65c2504872dd389
Lea Vaiana <leavaiana@gmail.com>
2018-12-17 17:02:01 +0100

    Changing style: addEventListener vs classList toggle

---
+++ Color_changerVsToggle/Color_changer.js
@@ -0,0 +1,23 @@
+var button = document.querySelector("button");
+var isPurple = false;
+
+/* button.addEventListener("click", function () {
+    if (isPurple) {
+        document.body.style.background = "white";
+        isPurple = false;
+    } else {
+        document.body.style.background = "purple";
+        isPurple = true;
+    }
+});
+*/
+
+button.addEventListener("click", function () {
+    if (isPurple) {
+        document.body.style.background = "white";
+    } else {
+        document.body.style.background = "purple";
+    } isPurple = !isPurple; //DYR code!
+});
+
+
