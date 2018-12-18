3bfe2ea2330ff4cbbf5c4f4e6ee6f7aa72a46cb1
Lea Vaiana <leavaiana@gmail.com>
2018-12-18 11:09:01 +0100

    Reset button added

--- scoreKeeper/ScoreKeeperLV.js
+++ scoreKeeper/ScoreKeeperLV.js
@@ -1,11 +1,15 @@
 var p1button = document.querySelector("#p1");
 var p2button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
 var winningScore = 5;
+
 
 p1button.addEventListener("click", function(){
 	if(!gameOver){
@@ -28,3 +32,18 @@ p2button.addEventListener("click", function(){
 		p2Display.textContent = p2Score;
 	}
 });
+
+resetButton.addEventListener("click", function(){
+	p1Score = 0;								//aggiorno JS
+	p2Score = 0;
+	p1Display.textContent = 0;					//aggiorno HTML
+	p2Display.textContent = 0;
+	p1Display.classList.remove("winner");		//rimuovo la classe winner per togliere il colore verde
+	p2Display.classList.remove("winner");
+	gameOver = false;							// necessario per riiniziare il gioco
+})
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+})
