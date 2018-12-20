cac471158e406a898dd2940934a419eaab70c2a7
Lea Vaiana <leavaiana@gmail.com>
2018-12-20 11:42:49 +0100

    New class added for improving JS code

--- Events for lists/List_events.js
+++ Events for lists/List_events.js
@@ -1,4 +1,4 @@
-/* CAMBIAMO LO STILE SUL PRIMO ELEMENTO DELLA LISTA
+/* 1) CAMBIAMO LO STILE SUL PRIMO ELEMENTO DELLA LISTA
 var firstLi = document.querySelector("li"); //seleziono il primo elemento della lista
 
 firstLi.addEventListener("mouseover", function(){
@@ -10,7 +10,7 @@ firstLi.addEventListener("mouseout", function(){
 });
 */
 
-//APPLICHIAMO LO STILE PRECEDENTE A TUTTI GLI ELEMENTI DELLA LISTA
+/* 2) APPLICHIAMO LO STILE PRECEDENTE A TUTTI GLI ELEMENTI DELLA LISTA
 var lis = document.querySelectorAll("li");
 
 for(var i = 0; i < lis.length; i ++){
@@ -21,6 +21,22 @@ for(var i = 0; i < lis.length; i ++){
         this.style.color="black";
     });
     //aggiungo lo stile (nel css) per un elemento da eliminare
+    lis[i].addEventListener("click", function(){
+        this.classList.toggle("done");
+    });
+}
+*/
+
+//3 MIGLIORIAMO IL CODICE PRECEDENTE GESTENDO LO STILE CON IL CSS.
+var lis = document.querySelectorAll("li");
+
+for(var i = 0; i < lis.length; i ++){
+    lis[i].addEventListener("mouseover", function(){
+        this.classList.add("selected");
+    });
+    lis[i].addEventListener("mouseout", function(){
+        this.classList.remove("selected");
+    });
     lis[i].addEventListener("click", function(){
         this.classList.toggle("done");
     });
